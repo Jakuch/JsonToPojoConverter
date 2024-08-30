326d0b8f4f2b5a053ed95e21ca0938b2395a6e6f
Jakuch <jakchr3@gmail.com>
2024-08-30 22:30:28 +0200

    removed currently not working feature

--- src/main/java/com/example/JsonToPojoConverter/controller/ConverterController.java
+++ src/main/java/com/example/JsonToPojoConverter/controller/ConverterController.java
@@ -2,11 +2,9 @@ package com.example.JsonToPojoConverter.controller;
 
 
 import com.example.JsonToPojoConverter.service.ConverterService;
-import com.example.JsonToPojoConverter.service.DownloadService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,7 +16,6 @@ public class ConverterController {
 
 
     private ConverterService converterService;
-    private DownloadService downloadService;
 
     @RequestMapping(value = {"/", "/home"})
     public String get() {
@@ -29,12 +26,7 @@ public class ConverterController {
     public String convert(@RequestParam(value = "file") MultipartFile file, @RequestParam(value = "name") String pojoName) throws IOException {
         converterService.convertJsonToJavaClass(file, pojoName);
 
-        return "redirect:/download";
-    }
-
-    @RequestMapping(value = "/download", method = RequestMethod.GET)
-    public String download() throws IOException {
-        downloadService.download();
         return "home";
     }
+
 }
